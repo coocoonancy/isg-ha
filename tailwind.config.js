21f190dc0bd060431f5fb101a33cdab75f87ea17
coocoonancy <chaonan.yang@broadlink.com.cn>
2024-10-14 18:02:24 +0800

    feat: 增加颜色定义

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,31 @@
 export default {
   prefix: 'lnl-',
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
-    colors: {
-      blue: '#378DF1',
-      purple: '#5C5AE3'
-    },
-    extend: {}
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        theme: '#007FFF',
+        divider: '#808695',
+        background: '#FFFFFF',
+        'body-background': '#F1F1F1',
+        primary: '#409EFF',
+        success: '#01C281',
+        warning: '#FABC20',
+        danger: '#FF4929',
+        info: '#909399',
+        text: '#515A6E',
+        'text-primary': '#17233D',
+        secondary: '#808695',
+        placeholder: '#B6BAC4',
+        border: '#DCDEE2',
+        'border-light': '#E8EAEC',
+        'border-lighter': '#EFEFEF',
+        'border-extralight': '#F8F8F9'
+      }
+    }
   },
   plugins: []
 };
