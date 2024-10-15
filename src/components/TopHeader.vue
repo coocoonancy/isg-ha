68b7346e49e0827061025732c2e93cae3c5ee114
coocoonancy <chaonan.yang@broadlink.com.cn>
2024-10-15 18:13:38 +0800

    feat: test vue-i18n

--- src/components/TopHeader.vue
+++ src/components/TopHeader.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
+  <h1>{{ $t('uiAdd') }}</h1>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
+import i18n from '@/i18n/index';
 import { isAppPlatform } from '@/utils/agent';
 if (isAppPlatform()) {
   document.documentElement.setAttribute('data-platform', 'app');
 } else {
   document.documentElement.setAttribute('data-platform', 'web');
 }
-createApp(App).mount('#app');
+const app = createApp(App);
+app.use(i18n);
+app.mount('#app');
